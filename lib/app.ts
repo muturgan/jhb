d7425632f1d8a14aaf6ee1cad78c1d00dc85c92b
Andrey Sakharov <caxap_8787@mail.ru>
2018-09-18 13:08:36 +0300

    cors was extended

--- lib/app.ts
+++ lib/app.ts
@@ -43,9 +43,10 @@ class App {
     private _config(): void {
         this.app.use((req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             next();
-          });
+        });
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
